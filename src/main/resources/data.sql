f4800df221fc48e503a6c7d8ba26407c8be4a40c
André Miyai <andremiyai@gmail.com>
2022-06-11 23:12:50 -0300

    Curso SpringBoot Api Rest: Profiles, Testes e Deploy Capitulo 1 - Authorization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$SM4LCQYElzlvS0UDWTxpFedjyCzt9YgLIv5S78Ld.2kI0WdowseNW');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$SM4LCQYElzlvS0UDWTxpFedjyCzt9YgLIv5S78Ld.2kI0WdowseNW');
+
+iNSERT INTO PERFIL(id,nome) VALUES(1, 'ROLE_ALUNO');
+iNSERT INTO PERFIL(id,nome) VALUES(2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) Values (1,1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) Values (2,2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
